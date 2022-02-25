6c8bcaeb25fe70f75b19b8827991f3b5f7c54eb4
Gabriel Lima <gabrielpk1@live.com>
2022-02-24 23:58:05 -0300

    add templete

--- README.md
+++
@@ -1,3 +0,0 @@
-# desafios-ignite
-Minhas soluções dos desafios do Ignite da Rocketseat <br/>
-As soluções estão nos Branches
--- webpack.config.js
+++ webpack.config.js
@@ -16,11 +16,11 @@ module.exports = {
     extensions: ['.js', '.jsx', '.ts', '.tsx']
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'public'),
+    static: path.resolve(__dirname, 'public'),
     hot: true,
   },
   plugins: [
-    isDevelopment && new ReactRefreshWebpackPlugin(),  
+    isDevelopment && new ReactRefreshWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html')
     })
@@ -43,6 +43,14 @@ module.exports = {
         test: /\.scss$/,
         exclude: /node_modules/,
         use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
       },
     ]
   }
