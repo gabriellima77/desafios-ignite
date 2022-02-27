035d76df511ae6a5c5bc9f15113b1b9d79c6d9ce
Gabriel Lima <gabrielpk1@live.com>
2022-02-27 19:52:58 -0300

    add challenge template

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,23 @@
-import { useEffect, useState } from 'react';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 
-import { SideBar } from './components/SideBar';
-import { Content } from './components/Content';
+import Routes from './routes';
+import GlobalStyles from './styles/global';
+import Header from './components/Header';
+import { CartProvider } from './hooks/useCart';
 
-import { api } from './services/api';
-
-import './styles/global.scss';
-
-import './styles/sidebar.scss';
-import './styles/content.scss';
-
-interface GenreResponseProps {
-  id: number;
-  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-  title: string;
-}
-
-export function App() {
-  const [selectedGenreId, setSelectedGenreId] = useState(1);
-
-  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>(
-    {} as GenreResponseProps
-  );
-
-  useEffect(() => {
-    api
-      .get<GenreResponseProps>(`genres/${selectedGenreId}`)
-      .then((response) => {
-        setSelectedGenre(response.data);
-      });
-  }, [selectedGenreId]);
-
-  function handleClickButton(id: number) {
-    setSelectedGenreId(id);
-  }
-
+const App = (): JSX.Element => {
   return (
-    <div style={{ display: 'flex', flexDirection: 'row' }}>
-      <SideBar
-        selectedGenreId={selectedGenreId}
-        handleClickButton={handleClickButton}
-      />
-      <Content selectedGenre={selectedGenre} selectedGenreId={selectedGenreId} />
-    </div>
+    <BrowserRouter>
+      <CartProvider>
+        <GlobalStyles />
+        <Header />
+        <Routes />
+        <ToastContainer autoClose={3000} />
+      </CartProvider>
+    </BrowserRouter>
   );
-}
+};
+
+export default App;
