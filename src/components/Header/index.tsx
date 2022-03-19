6085e023be8fa6706265eb808f94a94ac9e36a13
Gabriel Lima <gabrielpk1@live.com>
2022-03-19 18:14:47 -0300

    add Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,17 @@
-export default function Header() {
+import Link from 'next/link';
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
   // TODO
+  return (
+    <header className={styles.container}>
+      <div className={styles.content}>
+        <Link href="/">
+          <a className={styles.logo}>
+            <img src="Logo.png" alt="logo" />
+          </a>
+        </Link>
+      </div>
+    </header>
+  );
 }
