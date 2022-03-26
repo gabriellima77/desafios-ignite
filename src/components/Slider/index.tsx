18df38992982ff6c8fee4603bab9c071bd6468ac
Gabriel Lima <gabrielpk1@live.com>
2022-03-26 16:11:31 -0300

    add +100 cities

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -5,16 +5,25 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Box, Flex, Heading, Image, Text } from '@chakra-ui/react';
+import Link from 'next/link';
 
 const continents = [
   { continent: 'Europa', text: 'O continente mais antigo.' },
-  { continent: 'Americas', text: 'Paisagens Lindas.' },
+  { continent: 'America do Norte', text: 'Paisagens Lindas.' },
+  { continent: 'America do Sul', text: 'Um ambiente Tropical.' },
   { continent: 'Africa', text: 'O berço da humanidade.' },
   { continent: 'Asia', text: 'Um oriente há explorar.' },
   { continent: 'Oceania', text: 'Um novo mundo.' },
 ];
 
 export function Slider() {
+  const getContinentPath = (continent: string) => {
+    const path = continent.includes('America')
+      ? continent.replaceAll(' ', '_')
+      : continent;
+    return '/' + path.toLowerCase();
+  };
+
   return (
     <Swiper
       slidesPerView={1}
@@ -29,15 +38,19 @@ export function Slider() {
     >
       {continents.map(({ continent, text }) => (
         <SwiperSlide key={continent} style={{ listStyle: 'none' }}>
-          <Flex align="center" justify="center" position="relative">
-            <Image src="assets/Continent.png" alt={continent} />
-            <Box textAlign="center" position="absolute" color="gray.50">
-              <Heading fontSize={[20, 24, 48]}>{continent}</Heading>
-              <Text fontWeight={700} fontSize={[14, 18, 24]}>
-                {text}
-              </Text>
-            </Box>
-          </Flex>
+          <Link href={getContinentPath(continent)} passHref>
+            <a>
+              <Flex align="center" justify="center" position="relative">
+                <Image src="assets/Continent.png" alt={continent} />
+                <Box textAlign="center" position="absolute" color="gray.50">
+                  <Heading fontSize={[20, 24, 48]}>{continent}</Heading>
+                  <Text fontWeight={700} fontSize={[14, 18, 24]}>
+                    {text}
+                  </Text>
+                </Box>
+              </Flex>
+            </a>
+          </Link>
         </SwiperSlide>
       ))}
     </Swiper>
--- src/pages/[continent].tsx
+++ src/pages/[continent].tsx
@@ -1,11 +1,45 @@
-import { Box, Flex, Heading, Image } from '@chakra-ui/react';
+import { Box, Flex, Heading, Image, Text, Tooltip } from '@chakra-ui/react';
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Head from 'next/head';
+import { City } from '../components/City';
 import { Header } from '../components/Header';
 
 interface ContinentProps {
   continent: string;
 }
+
+const cities = [
+  {
+    name: 'Londres',
+    country: 'Reino Unido',
+    flag: 'assets/flag.png',
+    img: 'assets/londres.png',
+  },
+  {
+    name: 'Londres',
+    country: 'Reino Unido',
+    flag: 'assets/flag.png',
+    img: 'assets/londres.png',
+  },
+  {
+    name: 'Londres',
+    country: 'Reino Unido',
+    flag: 'assets/flag.png',
+    img: 'assets/londres.png',
+  },
+  {
+    name: 'Londres',
+    country: 'Reino Unido',
+    flag: 'assets/flag.png',
+    img: 'assets/londres.png',
+  },
+  {
+    name: 'Londres',
+    country: 'Reino Unido',
+    flag: 'assets/flag.png',
+    img: 'assets/londres.png',
+  },
+];
 
 export default function Continent({ continent }: ContinentProps) {
   return (
@@ -33,13 +67,77 @@ export default function Continent({ continent }: ContinentProps) {
             {continent}
           </Heading>
         </Flex>
+        <Flex
+          as="section"
+          p={[4, 18, 20, 24]}
+          direction={['column', 'column', 'column', 'row']}
+        >
+          <Text w={['100%', '100%', '100%', '50%']}>
+            A Europa é, por convenção, um dos seis continentes do mundo.
+            Compreendendo a península ocidental da Eurásia, a Europa geralmente
+            divide-se da Ásia a leste pela divisória de águas dos montes Urais,
+            o rio Ural, o mar Cáspio, o Cáucaso, e o mar Negro a sudeste
+          </Text>
+          <Flex
+            flex="1"
+            justify="space-around"
+            align="center"
+            mt={[8, 8, 8, 0]}
+          >
+            <Box textAlign="center">
+              <Heading color="yellow.500">50</Heading>
+              <Text>países</Text>
+            </Box>
+            <Box textAlign="center">
+              <Heading color="yellow.500">60</Heading>
+              <Text>línguas</Text>
+            </Box>
+            <Box textAlign="center">
+              <Heading color="yellow.500">27</Heading>
+              <Text display="flex" alignItems="center">
+                cidades + 100
+                <Tooltip label="Cidades mais visitadas do mundo">
+                  <Image
+                    display="inline-flex"
+                    src="assets/icons/Info.svg"
+                    alt="Informações"
+                    ml="1"
+                  />
+                </Tooltip>
+              </Text>
+            </Box>
+          </Flex>
+        </Flex>
+        <Box as="section" px="24">
+          <Heading fontSize={[24, 24, 24, 36]} fontWeight={500}>
+            Cidades +100
+          </Heading>
+          <Flex mt="8" wrap="wrap" gap="14">
+            {cities.map(({ name, country, img, flag }, i) => (
+              <City
+                key={name + i}
+                name={name}
+                country={country}
+                img={img}
+                flag={flag}
+              />
+            ))}
+          </Flex>
+        </Box>
       </Box>
     </>
   );
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const paths = ['europa', 'americas', 'oceania', 'asia', 'africa'];
+  const paths = [
+    'europa',
+    'america_do_norte',
+    'america_do_sul',
+    'oceania',
+    'asia',
+    'africa',
+  ];
   return {
     paths: paths.map((path) => ({ params: { continent: path } })),
     fallback: false,
@@ -48,6 +146,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps = async (context) => {
   let continent = String(context.params?.continent);
+  continent = continent.replaceAll('_', ' ');
   continent = continent[0].toUpperCase() + continent.substring(1);
   return {
     props: {
