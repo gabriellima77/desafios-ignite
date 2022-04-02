db79071cd484082205436b3a8def7180cdae7862
Gabriel Lima <gabrielpk1@live.com>
2022-04-02 17:34:46 -0300

    start at home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,18 +19,30 @@ export default function Home(): JSX.Element {
   } = useInfiniteQuery(
     'images',
     // TODO AXIOS REQUEST WITH PARAM
-    ,
+    async (pageParam = null) => {
+      const { data } = await api.get(`/api/images?after=${pageParam}`);
+      return data;
+    },
     // TODO GET AND RETURN NEXT PAGE PARAM
+    {
+      getNextPageParam: (lastPages, pages) => {
+        console.log(lastPages, pages);
+      },
+    }
   );
 
   const formattedData = useMemo(() => {
     // TODO FORMAT AND FLAT DATA ARRAY
+    const imageData = data.pages.map(page => {
+      return page.data;
+    });
+    return imageData.flat();
   }, [data]);
 
   // TODO RENDER LOADING SCREEN
-
+  if (isLoading) return <Loading />;
   // TODO RENDER ERROR SCREEN
-
+  if (isError) return <Error />;
   return (
     <>
       <Header />
@@ -38,6 +50,11 @@ export default function Home(): JSX.Element {
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
         {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button onClick={() => fetchNextPage()}>
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
